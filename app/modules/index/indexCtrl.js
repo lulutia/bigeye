a2208ee5164b64530c6e97de1cf8a4013fe91ef6
lulutia <nowillnowill@gmail.com>
2016-06-24 20:49:42 +0800

    feat(location): try to realize the data pass between the front-end and the node

--- app/modules/index/indexCtrl.js
+++ app/modules/index/indexCtrl.js
@@ -1,11 +1,25 @@
-define(['app', './indexService'], function(app){
+define(['app', './indexService', 'jquery'], function(app){
 	app.controller('indexCtrl', indexCtrl);
 	indexCtrl.$inject = ['$scope'];
-	
+
 	function indexCtrl($scope){
-		$scope.name = "just for test1";
+		$scope.name = "just for test1";	
 	}
 
+	$.ajax({   
+       type: 'post',  
+       url: '/app/index',   
+       data: {page:1},   
+       dataType: 'json',  
+       success:function (data){  
+           result = data; 
+           console.log(result); 
+       },    
+       error: function () {  
+           console.log("Save error!");  
+       }  
+  
+   });  
 	var html = "<div ng-controller='indexCtrl'><p>{{name}}</p></div>";
 	
 	return {_tpl: html }
--- index.js
+++ index.js
@@ -1,71 +1,44 @@
 var express = require('express');
 var cheerio = require('cheerio');
 var superagent = require('superagent');
-var app = express();
-var router = express.Router();
 var mongoose = require('mongoose');
 var EventProxy = require('eventproxy');
+
+var app = express();
+var router = express.Router();
 var ep = new EventProxy();
 var db = mongoose.connection;
+
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/myMovie');
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-	initData();
-});
+
 app.use(express.static(require('path').join(__dirname, 'app')));
 app.set('views', __dirname);
 app.set('view engine', 'html');
 app.engine('.html', require('ejs').__express);
 
-function initData() {
-	var topList = [];
+var topList = [];
+var MovieSchema;
+var MovieModel;
+var MovieSchema = new mongoose.Schema({
+	page: Number,
+	items: [{
+		name: String,
+		imgUrl: String,
+		other: String
+	}],
+});
+var MovieModel = db.model('Movie', MovieSchema);
+var initDb = false;
+
+db.once('open', function() {
 	for (var i = 0; i < 2; i++) {
 		var url = 'https://movie.douban.com/top250?start=' + (i * 25) + '&filter=';
 		topList.push(url);
-	}
-	var MovieSchema;
-	var MovieModel;
-	var MovieSchema = new mongoose.Schema({
-		page: Number,
-		items: [{
-			name: String,
-			imgUrl: String,
-			other: String
-		}],
-	});
-	var MovieModel = db.model('Movie', MovieSchema);
-	MovieModel.remove().exec();
+	};
 	app.get('/', function(req, res, next) {
-		topList.forEach(function(topUrl, index) {
-			superagent.get(topUrl)
-				.end(function(err, res) {
-					if (err) {
-						console.log('error');
-						return;
-					}
-					var items = [];
-					var $ = cheerio.load(res.text);
-					var title = $('.info .title:first-child');
-					var img = $('.pic img');
-					var otherInfo = $('.info .bd p');
-					for (var titlei = 0; titlei < title.length; titlei++) {
-						var item = {};
-						item.name = $(title[titlei]).text();
-						item.imgUrl = $(img[titlei]).attr('src');
-						item.other = $(otherInfo[titlei]).text();
-						items.push(item);
-					}
-					var moviePage = new MovieModel({
-						page: index,
-						items: items
-					});
-					moviePage.save(function(err) {
-						if (err) console.log(err);
-					});
-					ep.emit('getTopList');
-				})
-		})
+		initData();
 		ep.after('getTopList', topList.length, function() {
 			MovieModel.find({
 				page: 0
@@ -76,19 +49,67 @@ function initData() {
 				res.sendFile('main.html', {
 					root: __dirname + '/app'
 				});
-				app.listen(3000, function() {
-					console.log("done");
-				});
 			});
 		});
-	});
+	})
+})
+
+function initData() {
+	MovieModel.remove().exec();
+	topList.forEach(function(topUrl, index) {
+		superagent.get(topUrl)
+			.end(function(err, res) {
+				if (err) {
+					console.log('error');
+					return;
+				}
+				var items = [];
+				var $ = cheerio.load(res.text);
+				var title = $('.info .title:first-child');
+				var img = $('.pic img');
+				var otherInfo = $('.info .bd p');
+				for (var titlei = 0; titlei < title.length; titlei++) {
+					var item = {};
+					item.name = $(title[titlei]).text();
+					item.imgUrl = $(img[titlei]).attr('src');
+					item.other = $(otherInfo[titlei]).text();
+					items.push(item);
+				}
+				var moviePage = new MovieModel({
+					page: index,
+					items: items
+				});
+				moviePage.save(function(err) {
+					if (err) console.log(err);
+				});
+				ep.emit('getTopList');
+			})
+	})
+	initDb = true;
+
 }
 
 router.get('/[^?]*', function(req, res, next) {
+	if (!initDb) {
+		initData();		
+	} 
 	res.sendFile('main.html', {
-		root: __dirname + '/app'
-	});
+			root: __dirname + '/app'
+		});
 });
 
+router.post('/*',function(req, res){
+	if(req.url === '/app/index'){
+		MovieModel.find({page:0}).exec(function(err, doc){
+			res.json(doc);
+		})
+	}
+	console.log('ss' + req.url);
+})
 
 app.use('/', router);
+
+
+app.listen(3000, function() {
+	console.log("done");
+});
